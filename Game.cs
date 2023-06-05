4147b8e309a481a730f23bba8bca2941d08b3255
Истомин Дмитрий <112995250+SNOWIK1@users.noreply.github.com>
2023-06-05 15:42:19 +0700

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -6,10 +6,62 @@ using System.Threading.Tasks;
 
 namespace FINALLY_18_LABA
 {
-    public class Game
+    static internal class ListOfGames
     {
 
-        public string Title { get; set; }
-        public string Description {get; set;}
-        public string Price { get; set; }
+        static private List<Game> games = new List<Game>
+        {
+            new Game { Title = "Left 4 Dead", Price = "699 руб.",Description = "Многопользовательская компьютерная игра в жанрах шутера от первого лица и survival horror, разработанная американской компанией Turtle Rock Studios, которая с 10 января 2008 по 3 июня 2009 являлась частью Valve Corporation. Игра создана на движке Source версии 14 для платформ Microsoft Windows и Xbox 360. Портированием игры на Xbox 360 занималась студия Certain Affinity. 28 октября 2010 года игра стала доступна на macOS." }
+        };
+
+
+       static public void AddGame(Game game)
+        {
+            games.Add(game);
+        }
+
+        static public void RemoveGame(string title)
+        {
+            foreach (Game game in games)
+            {
+                if (game.Title == title)
+                {
+                    games.Remove(game);
+                    break;
+                }
+            }
+        }
+
+        static public void UpdateGame(string title, string desc, string price)
+        {
+            Game game = null;
+            Game pre = null;
+
+            foreach (Game g in games) 
+            {
+                if (g.Title == title)
+                {
+                    game = g;
+                    pre = g;
+                    break;
+                }
+            }
+
+            game.Title = title;
+            game.Description = desc;
+            game.Price = price;
+
+            games.Remove(pre);
+            games.Add(game);
+
+        }
+
+        static public List<Game> GetGames()
+        {
+            return games;
+        }
+
     }
+
+}
+
