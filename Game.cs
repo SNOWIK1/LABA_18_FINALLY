d72b86a913f20b01ca3a0292fbea259b133ff1a7
Истомин Дмитрий <112995250+SNOWIK1@users.noreply.github.com>
2023-06-05 15:41:59 +0700

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,41 +8,8 @@ namespace FINALLY_18_LABA
 {
     public class Game
     {
-        private List<string> genres = new List<string>();
+
         public string Title { get; set; }
-
-        public void AddGenres(string[] arr)
-        {
-            foreach (string g in arr)
-            {
-                genres.Add(g);
-            }
-        }
-
-        public void Update(string[] arr)
-        {
-            foreach (string g in genres)
-            {
-                genres.Remove(g);
-            }
-            
-            foreach (string g in arr)
-            {
-                genres.Add(g);
-            }
-
-        }
-
-        public string GetGenres()
-        {
-            string result = "";
-
-            foreach (string g in genres)
-            {
-                result += (g + " ");
-            }
-            return result;
-        }
         public string Description {get; set;}
+        public string Price { get; set; }
     }
-}
