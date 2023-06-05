6ac6468c99100644fcf3c300098822e12faca57a
Истомин Дмитрий <112995250+SNOWIK1@users.noreply.github.com>
2023-06-05 15:43:42 +0700

    Update ListOfGames.cs

--- ListOfGames.cs
+++ ListOfGames.cs
@@ -1,25 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FINALLY_18_LABA
-{   
+{
     static internal class ListOfGames
     {
-      
+
         static private List<Game> games = new List<Game>
         {
-            new Game { Title = "Left 4 Dead", Description = "Многопользовательская компьютерная игра в жанрах шутера от первого лица и survival horror, разработанная американской компанией Turtle Rock Studios, которая с 10 января 2008 по 3 июня 2009 являлась частью Valve Corporation. Игра создана на движке Source версии 14 для платформ Microsoft Windows и Xbox 360. Портированием игры на Xbox 360 занималась студия Certain Affinity. 28 октября 2010 года игра стала доступна на macOS." };
-    };
+            new Game { Title = "Left 4 Dead", Price = "699 руб.",Description = "Многопользовательская компьютерная игра в жанрах шутера от первого лица и survival horror, разработанная американской компанией Turtle Rock Studios, которая с 10 января 2008 по 3 июня 2009 являлась частью Valve Corporation. Игра создана на движке Source версии 14 для платформ Microsoft Windows и Xbox 360. Портированием игры на Xbox 360 занималась студия Certain Affinity. 28 октября 2010 года игра стала доступна на macOS." }
+        };
 
-       static public void AddGame (Game game)
+
+       static public void AddGame(Game game)
         {
             games.Add(game);
         }
 
-        static public void RemoveGame (string title)
+        static public void RemoveGame(string title)
         {
             foreach (Game game in games)
             {
@@ -31,7 +32,7 @@ namespace FINALLY_18_LABA
             }
         }
 
-        static public void UpdateGame(string title, string desc, string genre)
+        static public void UpdateGame(string title, string desc, string price)
         {
             Game game = null;
             Game pre = null;
@@ -48,7 +49,7 @@ namespace FINALLY_18_LABA
 
             game.Title = title;
             game.Description = desc;
-            game.Genre.Add(genre);
+            game.Price = price;
 
             games.Remove(pre);
             games.Add(game);
@@ -61,4 +62,5 @@ namespace FINALLY_18_LABA
         }
 
     }
+
 }
